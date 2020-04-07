e9b33837bf2d501d537af73395b148f6316fff64
laborci <gergely@laborci.hu>
2020-04-07 16:11:25 +0200

    minor changes

--- @src/application/Startup.php
+++ @src/application/Startup.php
@@ -10,12 +10,24 @@ use Andesite\Util\ErrorHandler\ErrorHandler;
 use Andesite\Util\ErrorHandler\ExceptionHandlerInterface;
 use Andesite\Util\ErrorHandler\FatalErrorHandlerInterface;
 use Andesite\DBAccess\Connection\SqlLogInterface;
+use Andesite\Util\RemoteLog\RemoteLogSender;
+use Symfony\Component\HttpFoundation\Request;
 
 class Startup extends Module{
 
 	public static function run($config){
-		if(Andesite::Service()->isDevMode()){
-			$remoteLog = new RemoteLog($config['remote-log-host'], Andesite::Service()->getRequestId());
+		if (Andesite::Service()->isDevMode()){
+			$request = ServiceContainer::get(Request::class);
+			$requestId = Andesite::Service()->getRequestId();
+			$remoteLog = new RemoteLog(
+				new RemoteLogSender(
+					$config['remote-log-host'],
+					$requestId,
+					$request->getMethod(),
+					$request->getHost(),
+					$request->getPathInfo()
+				)
+			);
 			ServiceContainer::value(ExceptionHandlerInterface::class, $remoteLog);
 			ServiceContainer::value(FatalErrorHandlerInterface::class, $remoteLog);
 			ServiceContainer::value(DumpInterface::class, $remoteLog);
