ab10e7b07dc446339c4f01333be6e03124b8ec4f
laborci <gergely@laborci.hu>
2020-04-07 13:47:08 +0200

    new rlog

--- @src/application/Startup.php
+++ @src/application/Startup.php
@@ -1,23 +1,28 @@
 <?php namespace Application;
 
 use Andesite\Core\Boot\Andesite;
-use Andesite\Core\Env\Env;
 use Andesite\Core\Module\Module;
 use Andesite\Core\ServiceManager\ServiceContainer;
-use Andesite\DBAccess\Connection\SqlLogHookInterface;
+use Andesite\Util\Dumper\Dumper;
+use Andesite\Util\Dumper\DumpInterface;
 use Andesite\Util\RemoteLog\RemoteLog;
-use Andesite\Util\RemoteLog\SqlLogHook;
+use Andesite\Util\ErrorHandler\ErrorHandler;
+use Andesite\Util\ErrorHandler\ExceptionHandlerInterface;
+use Andesite\Util\ErrorHandler\FatalErrorHandlerInterface;
+use Andesite\DBAccess\Connection\SqlLogInterface;
 
 class Startup extends Module{
 
 	public static function run($config){
 		if(Andesite::Service()->isDevMode()){
-			ServiceContainer::value(RemoteLog::class, new RemoteLog($config['remote-log-host']));
-			ServiceContainer::shared(SqlLogHookInterface::class, SqlLogHook::class);
-			RemoteLog::loadFacade();
-		}else{
-			RemoteLog::loadFakeFacade();
+			$remoteLog = new RemoteLog($config['remote-log-host'], Andesite::Service()->getRequestId());
+			ServiceContainer::value(ExceptionHandlerInterface::class, $remoteLog);
+			ServiceContainer::value(FatalErrorHandlerInterface::class, $remoteLog);
+			ServiceContainer::value(DumpInterface::class, $remoteLog);
+			ServiceContainer::value(SqlLogInterface::class, $remoteLog);
+			ErrorHandler::Service()->register();
 		}
+		Dumper::load();
 	}
 
 }
